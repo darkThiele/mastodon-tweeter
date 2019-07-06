885d47426fdabf158453013045aa4ee80808d9b6
Hatsunori <hatsunri.kashima@gmail.com>
2019-07-06 10:14:39 +0900

    Updates

---
+++ precache-manifest.e49434649fe9cb33a5e5e3d3889dab29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d81bcd0a7d452e99976087862e8e0f",
+    "url": "/mastodon-tweeter/index.html"
+  },
+  {
+    "revision": "0e461563d5c3ebba07ca",
+    "url": "/mastodon-tweeter/static/css/main.0c9f06a1.chunk.css"
+  },
+  {
+    "revision": "1543f9a98e008b277df1",
+    "url": "/mastodon-tweeter/static/js/2.a530017a.chunk.js"
+  },
+  {
+    "revision": "0e461563d5c3ebba07ca",
+    "url": "/mastodon-tweeter/static/js/main.9eba9722.chunk.js"
+  },
+  {
+    "revision": "0bd017d2842b23000475",
+    "url": "/mastodon-tweeter/static/js/runtime~main.79678d17.js"
+  }
+]);
